4d9310c432a51c0d22a16bec120329ac11cb9423
PerejilTriste <ungunovoleg@gmail.com>
2023-06-21 23:35:17 +0300

    test menu

--- bin/menu.js
+++ bin/menu.js
@@ -18,3 +18,21 @@ const content = fs.readFileSync(path.join(
 ), 'utf-8');
 
 showInfo(content);
+
+const allPositions = content.length-1
+//просмотр меню
+// console.log(allPositions);
+
+// массив с выбранными позициями
+let finalOrder = [1, 3, 5];
+
+function arraySum(array){
+let sum = 0;
+for(var i = 0; i < array.length; i++){
+    sum += array[i];
+    }
+console.log(`Итоговая сумма: ${sum}`);
+}
+
+
+
